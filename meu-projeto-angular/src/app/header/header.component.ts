ecee5f43fcc4d6e9f1bab084a25011a09dfb7620
Eliseu <eliseu.souza@mail.usf.edu.br>
2024-07-12 11:24:29 -0300

    aidicionando a pagina do carrinho

--- meu-projeto-angular/src/app/header/header.component.ts
+++ meu-projeto-angular/src/app/header/header.component.ts
@@ -1,15 +1,19 @@
 import { CarrinhoService } from './../carrinho.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent  implements OnInit{
 
 
     constructor(
       public CarrinhoService: CarrinhoService
     ){}
+
+    ngOnInit(): void {
+
+    }
 }
